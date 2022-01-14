8d3f990649be1d28cae3a0fa6b9e7a912e51b6a0
GUPTA-JI <guptapriyanshu2001@gmail.com>
2022-01-15 01:42:12 +0530

    make it better

--- app.js
+++ app.js
@@ -23,11 +23,12 @@ app.use(require("./routes/auth"));  //import route from file and register them u
 app.use(require('./routes/post'));
 app.use(require('./routes/user'));
 
-if(process.env.NODE_ENV=="production"){
-    app.use(express.static("client/buid"))
-    const path = require('path')
-    app.get("*",(req,res)=>{
-        res.sendFile(path.resolve(__dirname,"client","build","index.html"))
+if (process.env.NODE_ENV === "production") {
+
+    app.use(express.static('client/built'));
+    app.get("*", (req, res) => {
+        res.sendFile(require('path')
+            .resolve(__dirname, 'client', 'build', 'index.html'));
     })
 }
 
