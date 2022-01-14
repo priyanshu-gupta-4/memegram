25e322d81b5c3eca71fdbc30d5eabbb6932a6eec
GUPTA-JI <guptapriyanshu2001@gmail.com>
2022-01-15 01:13:22 +0530

    make it better

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ if(process.env.NODE_ENV=="production"){
     app.use(express.static("client/buid"))
     const path = require('path')
     app.get("*",(req,res)=>{
-        res.sendFile(path.resolve(__dirname,"client","buid","index.html"))
+        res.sendFile(path.resolve(__dirname,"client","build","index.html"))
     })
 }
 
