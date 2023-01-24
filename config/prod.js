e485b943aa982eea1cfb5ca610ae6ab9312fd479
GUPTA-JI <guptapriyanshu2001@gmail.com>
2023-01-24 19:50:29 +0530

    fixed a bug

--- config/prod.js
+++ config/prod.js
@@ -2,5 +2,6 @@ module.exports = {
     MONGOURL : process.env.MONGOURL,
     JWT_SECRET:process.env.JWT_SEC,
     Gmail:process.env.GMAIL,
-    pass:process.env.PASS
+    pass:process.env.PASS,
+    openaikey:process.env.openaikey
 }
