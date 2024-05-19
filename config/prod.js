ec2691dfb4c73e4734a9173a8b6bd891e3655602
iamjha007 <iamjha007@gmail.com>
2024-05-19 17:10:01 +0530

    dev change

--- config/prod.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    MONGOURL : process.env.MONGOURL,
-    JWT_SECRET:process.env.JWT_SEC,
-    Gmail:process.env.GMAIL,
-    pass:process.env.PASS,
-    openaikey:process.env.OPENAI,
-    sendgridKey:process.env.SENDGRID
-}
--- sockets/index.js
+++
@@ -1,9 +0,0 @@
-const io = require("socket.io")(8900,{
-    cors:{
-        origin:"*"
-    }
-});
-
-io.on("connection",(socket)=>{
-    console.log("a user connected "+socket);
-})
